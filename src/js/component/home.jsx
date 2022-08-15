722edf1f655d7854022cdcf48d43a2b1978f1f2b
Deby92 <d.cantuarias.urrutia@gmail.com>
2022-08-15 22:08:37 +0000

    lunes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import { BsFillTrashFill } from "react-icons/bs";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -10,7 +11,7 @@ const Home = () => {
 
 	const addItem = () => {
 		if(!newItem) {
-			alert("enter an item.")
+			alert("enter an item.");
 			return;
 		}
 
@@ -18,9 +19,17 @@ const Home = () => {
 			id: Math.floor(Math.random() * 1000),
 			value: newItem
 		};
+
+		setItems(oldList => [...oldList, item]);
+		setNewItem("");
+	}
+	const deleteItem = (id) => {
+		const newArray = items.filter(item => item.id !== id);
+		setItems(newArray);
+
 	}
 	return (
-		<div className="App">
+		<div className="App justify-content-center my-5">
 			<h1 className="text">ToDo list App!</h1>
 
 			<input 
@@ -30,6 +39,14 @@ const Home = () => {
 				onChange={e => setNewItem(e.target.value)}
 			/>
 			<button onClick={() => addItem()}>Add</button>
+
+			<ul>
+				{items.map(item => {
+					return(
+						<li key={item.id}>{item.value} <button onClick={() => deleteItem(item.id)}><BsFillTrashFill/></button></li>
+					)
+				})}
+			</ul>
 			
 		</div>
 	);
