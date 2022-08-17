b9fd8eefffea76cfdd249868b98a7b42aeb0a6f0
Deby92 <d.cantuarias.urrutia@gmail.com>
2022-08-17 19:13:19 +0000

    detalles btn y edit function

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,67 +1,94 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { BsFillTrashFill } from "react-icons/bs";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function Home() {
+	const [todos, setTodos] = useState([]);
+	const [todo, setTodo] = useState("");
+	const [todoEditing, setTodoEditing] = useState(null);
+	const [editingText, setEditingText] = useState("");
 
-//create your first component
-const Home = () => {
-	const [newItem, setNewItem] = useState("");
-	const [items, setItems] = useState([]);
-	
-
-	const addItem = () => {
-		if(!newItem) {
-			alert("enter an item.");
-			return;
-		}
-
-		const item = {
-			id: Math.floor(Math.random() * 1000),
-			value: newItem
+	function handleSubmit(e) {
+		e.preventDefault();
+		const newTodo = {
+			id: new Date().getTime(),
+			text: todo,
+			completed: false,
 		};
-
-		setItems(oldList => [...oldList, item]);
-		setNewItem("");
+		setTodos([...todos].concat(newTodo));
+		setTodo("");
 	}
-	const deleteItem = (id) => {
-		const newArray = items.filter(item => item.id !== id);
-		setItems(newArray);
 
+	function deleteTodo(id) {
+		const updateTodos = [...todos].filter((todo) => todo.id !== id);
+		setTodos(updateTodos);
 	}
+
+	function editTodo(id) {
+		const updateTodos = [...todos].map((todo) => {
+			if (todo.id === id) {
+				todo.id = editingText;
+			}
+			return todo;
+		});
+		setTodos(updateTodos);
+		setTodoEditing(null);
+		setEditingText("");
+	}
+
 	return (
-		<div className="App App d-flex container-fluid row justify-content-center mt-5" style={{
-			borderRadius: "4%",
-			border: "3px solid black",
-			backgroundColor: "cyan",
-			width: "500px",
-			margin: "auto",
-			listStyle: "none",
-		}}>
-			<h1 className="text">ToDo list App!</h1>
-
-			<input 
-				className="input text-center p-4"
-				type="text"
-				placeholder="Add Item here"
-				value={newItem}
-				onChange={e => setNewItem(e.target.value)}
-				
-			/>
-			<button 
-			className="btn_container"
-			onClick={() => addItem()}>Add</button>
-
-			<ul className="list-group list-group-flush d-flex">
-				{items.map(item => {
-					return(
-						<li key={item.id}>{item.value} <button className="list-group-item float-end bg-transparent" onClick={() => deleteItem(item.id)}><BsFillTrashFill/></button></li>
-					)
-				})}
-			</ul>
-			
+		<div
+			className="App d-flex container-fluid row justify-content-center mt-5"
+			style={{
+				borderRadius: "9%",
+				border: "3px solid black",
+				backgroundColor: "#BFE6BA",
+				width: "500px",
+				margin: "auto",
+			}}>
+			<h1>{"Todo List"}</h1>
+			<form onSubmit={handleSubmit}>
+				<input
+					type={"text"}
+					onChange={(e) => setTodo(e.target.value)}
+					value={todo}
+				/>
+				<button className="btn btn btn-outline-dark  border-secondary m-1" type="submit">
+					Add
+				</button>
+			</form>
+			{todos.map((todo) => (
+				<div className="todoKey" key={todo.id}>
+					{todoEditing === todo.id ? (
+						<input
+							className="d-flex row"
+							type={"text"}
+							onChange={(e) => setEditingText(e.target.value)}
+							value={editingText}
+						/>
+					) : (
+						<div className="d-flex row d-flex float">
+							<div
+								style={{ fontSize: "20px" }}
+								className="todoText">
+								{" " + todo.text}
+							</div>
+						</div>
+					)}
+					<button
+						className="btn delete-btn btn-info m-1 float-end"
+						onClick={() => deleteTodo(todo.id)}>
+						<BsFillTrashFill />
+					</button>
+					<button
+						className="m-1 btn btn-outline-dark btn-sm float-end"
+						onClick={() => setTodoEditing(todo.id)}>
+						Edit
+					</button>
+					
+				</div>
+			))}
 		</div>
 	);
-};
+}
 
 export default Home;
